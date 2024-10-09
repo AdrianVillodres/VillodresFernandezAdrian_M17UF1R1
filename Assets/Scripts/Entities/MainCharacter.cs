575d5c2860473303e3db567f7e62afaa2256a0e5
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2024-10-09 16:36:45 +0200

    added sprites and tiles and first animation done

--- Assets/Scripts/Entities/MainCharacter.cs
+++ Assets/Scripts/Entities/MainCharacter.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MainCharacter : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-3f, 0) * Time.deltaTime;
+            spriteRenderer.flipX = true;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(3f, 0) * Time.deltaTime;
+            spriteRenderer.flipX = false;
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("Running", true);
+        }
+        else
+        {
+            animator.SetBool("Running", false);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().gravityScale *= -1;
+            if (spriteRenderer.flipY)
+            {
+                spriteRenderer.flipY = false;
+            }
+            else
+            {
+                spriteRenderer.flipY = true;
+            }
+
+        }
+
     }
 }
