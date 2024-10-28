d360cfda032760343c24befe5b47bcbebccfb3d4
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2024-10-28 17:53:15 +0100

    enemy updated

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private Rigidbody2D _rb;
-    [SerializeField] private float _speed;
+    public Rigidbody2D _rb;
+    int _speed = 1;
+    private SpriteRenderer spriteRenderer;
     void Awake()
     {
-        _rb = GetComponent<Rigidbody2D>();
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+        spriteRenderer.flipX = true;
     }
 
     void Update()
     {
+        _rb.velocity = new Vector2(-_speed, 0);
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("TurnEnemy"))
+        {
+            if (spriteRenderer.flipX == true)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else
+            {
+                spriteRenderer.flipX = true;
+            }
+            
+        }
     }
 }
